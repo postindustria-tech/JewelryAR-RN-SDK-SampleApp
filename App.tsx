b71fe221019475afe0ed2e74746e9487f3c6031b
Maxim Kucherov <maximus.the0@gmail.com>
2022-06-09 13:32:32 +0300

    Use our package to show the ring on the second screen.

--- App.tsx
+++ App.tsx
@@ -1,23 +1,33 @@
 import * as React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { ArView } from 'jewelry-ar-react-native-sdk';
+
+
 
 const Stack = createNativeStackNavigator();
 
 const HomeScreen = ({ navigation }) => {
   return (
     <Button
-      title="Go to Jane's profile"
+      title="Open AR View"
       onPress={() =>
-        navigation.navigate('Profile', { name: 'Jane' })
+        navigation.navigate('AR', { modelId: '73' })
       }
     />
   );
 };
 
-const ProfileScreen = ({ navigation, route }) => {
-  return <Text>This is {route.params.name}'s profile</Text>;
+const ArScreen = ({ navigation, route }) => {
+  return (
+    <ArView
+      style={{ position: 'absolute', left: 0, right: 0, top: 0, bottom: 0 }}
+      host="https://stage-api-ar.postindustria.com/v1"
+      token="725b5078594e851aff1186b14fd46aa05bf8dc498a999ee6619f4defaf9d9f15"
+      modelId={route.params.modelId}
+    />
+  );
 };
 
 const App = () => {
@@ -29,7 +39,7 @@ const App = () => {
           component={HomeScreen}
           options={{ title: 'Welcome' }}
         />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="AR" component={ArScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
